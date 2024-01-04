48e440e15704a6ceda98a7adf317cc0dc9957ff9
stwardak <148924249+stwardak@users.noreply.github.com>
2024-01-04 14:12:48 -0600

    added index action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,31 @@
- import { RoomsIndex } from "./RoomsIndex";
+import { RoomsIndex } from "./RoomsIndex";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
   export function Content() {
+    const rooms = [
+      {id: 1, user_id: 2, address: "123 main st", city: "Chicago", state: "IL", price: 300, description: "king", home_type: "hotel", room_type: "king", total_occupancy: 4, total_bedrooms: 1, total_bathrooms: 1},
+      {id: 2, user_id: 3, address: "123 main st", city: "San Diego", state: "CA", price: 350, description: "hut", home_type: "beach hut", room_type: "double", total_occupancy: 4, total_bedrooms: 1, total_bathrooms: 1}
+     ];
+
+
+    //  web request not working yet - need api 
+// const [rooms, setRooms] = useState([]);
+
+//    const handleIndexRooms = () => {
+//      console.log("handleIndexRooms");
+//      axios.get("http://localhost:3000/rooms.json").then((response) => {
+//        console.log(response.data);
+//        setRooms(response.data);
+//      });
+//    };
+
+//    useEffect(handleIndexRooms, []);
+
+
     return (
       <div>
-       <RoomsIndex />
+       <RoomsIndex rooms={rooms}/>
       </div>
     );
   }
--- src/RoomsIndex.jsx
+++ src/RoomsIndex.jsx
@@ -1,8 +1,23 @@
-
-export function RoomsIndex() {
-  return (
-    <div>
-      <h1>All Rooms</h1>
-    </div>
-  );
-}
+  export function RoomsIndex(props) {
+      return (
+        <div>
+          <h1>All rooms</h1>
+           {props.rooms.map((room) => (
+             <div key={room.id}>
+               <h2>{room.id}</h2>
+               <p>{room.user_id}</p>
+               <p>{room.address}</p>
+               <p>{room.city}</p>
+               <p>{room.state}</p>
+               <p>{room.price}</p>
+               <p>{room.description}</p>
+               <p>{room.home_type}</p>
+               <p>{room.room_type}</p>
+               <p>{room.total_occupancy}</p>
+               <p>{room.total_bedrooms}</p>
+               <p>{room.total_bathrooms}</p>
+             </div>
+           ))}
+        </div>
+      );
+    }
