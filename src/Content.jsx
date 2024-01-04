34c8f3d6a32053ab7c73e545cea06c245d4b528c
stwardak <148924249+stwardak@users.noreply.github.com>
2024-01-04 16:02:04 -0600

    turned off prop types lintr error

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,24 +6,23 @@ import { RoomsShow } from "./RoomsShow";
 
 
   export function Content() {
-    const rooms = [
-      {id: 1, user_id: 2, address: "123 main st", city: "Chicago", state: "IL", price: 300, description: "king", home_type: "hotel", room_type: "king", total_occupancy: 4, total_bedrooms: 1, total_bathrooms: 1},
-      {id: 2, user_id: 3, address: "123 main st", city: "San Diego", state: "CA", price: 350, description: "hut", home_type: "beach hut", room_type: "double", total_occupancy: 4, total_bedrooms: 1, total_bathrooms: 1}
-     ];
+  //   const rooms = [
+  //     {id: 1, user_id: 2, address: "123 main st", city: "Chicago", state: "IL", price: 300, description: "king", home_type: "hotel", room_type: "king", total_occupancy: 4, total_bedrooms: 1, total_bathrooms: 1},
+  //     {id: 2, user_id: 3, address: "123 main st", city: "San Diego", state: "CA", price: 350, description: "hut", home_type: "beach hut", room_type: "double", total_occupancy: 4, total_bedrooms: 1, total_bathrooms: 1}
+  //    ];
 
 
-    //  web request not working yet - need api 
-// const [rooms, setRooms] = useState([]);
+const [rooms, setRooms] = useState([]);
 const [isRoomsShowVisible, setIsRoomsShowVisible] = useState(false);
 const [currentRoom, setCurrentRoom] = useState ({});
 
-//    const handleIndexRooms = () => {
-//      console.log("handleIndexRooms");
-//      axios.get("http://localhost:3000/rooms.json").then((response) => {
-//        console.log(response.data);
-//        setRooms(response.data);
-//      });
-//    };
+   const handleIndexRooms = () => {
+     console.log("handleIndexRooms");
+     axios.get("http://localhost:3000/rooms.json").then((response) => {
+       console.log(response.data);
+       setRooms(response.data);
+     });
+   };
 
 const handleShowRoom = (room) => {
   console.log("handleShowRoom", room);
@@ -37,7 +36,7 @@ const handleShowRoom = (room) => {
       setIsRoomsShowVisible(false);
     };
 
-  //  useEffect(handleIndexRooms, []);
+   useEffect(handleIndexRooms, []);
 
 
     return (
